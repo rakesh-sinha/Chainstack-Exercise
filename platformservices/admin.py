762ea829ddbf84c3f8a5268ee2e13b2efd68b254
Rakesh Sinha <rakeshsinha31@gmail.com>
2019-04-13 12:07:14 +0800

    Initial  Commit

---
+++ platformservices/admin.py
@@ -0,0 +1,35 @@
+"""Integrate with admin module."""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import User, Resource
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password','quota')}),
+    )
+    list_display = ('email', 'quota', 'is_staff')
+    ordering = ('email',)
+
+
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('res_name', 'created_by')
+    exclude = ['created_by',]
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(created_by=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Resource, ResourceAdmin)
